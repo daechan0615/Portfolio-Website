5a9edf26b47e819d0fc8aa793c77eb3353bab4e1
daechan0615 <d.eom@mail.utoronto.ca>
2025-09-28 19:49:52 -0400

    Complete footer.:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import AboutSection from "../components/AboutSection";
 import SkillsSection from "../components/SkillsSection";
 import ProjectSection from "../components/ProjectSection";
 import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -25,6 +26,7 @@ const Home = () => {
         <ContactSection />
       </main>
       {/* Footer */}
+      <Footer />
     </div>
   );
 };
