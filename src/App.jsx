911a800873719cd68f35fb255bf22c9f47317edb
daechan0615 <d.eom@mail.utoronto.ca>
2025-09-28 22:51:38 -0400

    Add Vercel Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <>
+      <SpeedInsights />
       <Toaster />
       <BrowserRouter>
         <Routes>
